4487efb89e61c3a2bf137d582b0723b842380399
parfait2 <parkyl0208@naver.com>
2023-04-12 18:40:59 +0900

    Pk번째수_박예린

---
+++ DAY03/Pk번째수/Pk번째수_박예린.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+    tmp = []
+    
+    for i in range(len(commands)):
+        tmp = array[(commands[i][0]-1):commands[i][1]]
+        tmp.sort()
+        answer.append(tmp[commands[i][2]-1])
+
+    return answer
