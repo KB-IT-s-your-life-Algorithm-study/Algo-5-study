67911aace2101dc796409a23bb1a69570baf9507
seoeunhyo <seoeunhyo@a.ut.ac.kr>
2023-04-12 19:07:53 +0900

    P_서은효 sol

--- DAY03/Pk번째수/Pk번째수_서은효.java
+++ DAY03/Pk번째수/Pk번째수_서은효.java
@@ -1,10 +1,42 @@
 package Pk번째수;
 
+import java.util.Arrays;
+
 public class Pk번째수_서은효 {
+	
+	static int index;
+	
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] answer;
+        //[[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+        answer = new int[commands[0].length];
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        for(int i =0,len= commands[0].length ; i<len;i++) {
+        	
+        	int first = commands[i][0];
+        	int last = commands[i][1];
+        	int k = commands[i][2];
+        	
+        	int [] arr = new int[last - first + 1];
+        	
+        	for(int j = first-1, p=0; j<last;j++,p++) {
+        		arr[p] = array[j]; 	
+        	}
+        	
+        	Arrays.sort(arr);
+        	
+        	answer[i] = arr[k-1];
+        	
+        }
+        
+        return answer;
+    }
 
+    public static void main(String[] args) {
+		int arr [] = {1, 5, 2, 6, 3, 7, 4};
+		int com [][] = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+		int s[] = solution(arr, com);
+	
 	}
 
 }
--- DAY03/P같은숫자는싫어/P같은숫자는싫어_서은효.java
+++ DAY03/P같은숫자는싫어/P같은숫자는싫어_서은효.java
@@ -1,10 +1,40 @@
 package P같은숫자는싫어;
 
+import java.util.Stack;
+
 public class P같은숫자는싫어_서은효 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+
+    public static int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(int x : arr) {
+        	if(stack.size() == 0)
+        		stack.push(x);
+        	if(stack.peek() != x ) {
+        		stack.push(x);
+        	}
+        		
+        }
+        int[] answer = new int[stack.size()];
+       
+        
+        for(int i =answer.length-1 ; i>=0; i--) {
+        	 
+        	answer[i] = stack.pop();
+        }
+        
+        return answer;
+       
+    }
+
+
+    public static void main(String[] args) {
+	int arr2[] = {1,1,3,3,0,1,1};
+	//System.out.println((arr2));
+	
+	System.out.println(solution(arr2));
+}
 
 }
