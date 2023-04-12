15e574aeca9af577b81552a474d63ba1f09dc6e8
seoeunhyo <seoeunhyo@a.ut.ac.kr>
2023-04-12 17:53:51 +0900

    day03 uplaod

---
+++ DAY03/Pk번째수/Pk번째수_서은효.java
@@ -0,0 +1,10 @@
+package Pk번째수;
+
+public class Pk번째수_서은효 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DAY03/P같은숫자는싫어/P같은숫자는싫어_서은효.java
@@ -0,0 +1,10 @@
+package P같은숫자는싫어;
+
+public class P같은숫자는싫어_서은효 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
