023465a6abab3fc9872fc6f6b6f21a64217bc1bf
Chos1 <chosw0812@naver.com>
2023-04-26 17:49:41 +0900

    DAY05_조성환

---
+++ DAY05/P조이스틱/P조이스틱_조성환.java
@@ -0,0 +1,82 @@
+package tset;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class P조이스틱_조성환 {
+
+	public static void main(String[] args) {
+
+		int n = 5;
+		int[] lost = new int[] {2,4};
+		int[] reserve = new int[] {3};
+		
+		System.out.println(solution(n, lost, reserve));
+	}
+	
+	 public static int solution(int n, int[] lost, int[] reserve) {
+		 	
+		 	// 결국 체육복을 못 빌린 사람의 수
+		 	int no_wear = 0;	
+		 	// 체육복이 필요한 사람
+		 	List<Integer> lost_man = new ArrayList<>();
+		 	// 체육복이 남는 사람
+		 	List<Integer> sub_man = new ArrayList<>();
+		 	
+		 	// 옷 상태
+		 	int[] wear_on = new int[n+1];
+		 	
+		 	// 정렬
+		 	Arrays.sort(lost);
+		 	Arrays.sort(reserve);
+		 	
+		 	// 여벌 옷이 있는 사람
+		 	for(int i = 0 ; i < reserve.length ; i++) {
+		 		wear_on[reserve[i]] += 1;
+		 	}
+		 	
+		 	// 훔쳐진 사람
+		 	for(int i = 0 ; i < lost.length ; i++) {
+		 		// 여벌이 있으면 입는다.
+		 		if(wear_on[lost[i]] != 0) {		 			
+		 			wear_on[lost[i]] -= 1;
+		 		// 없으면 없는 사람으로
+		 		}else {
+		 			lost_man.add(lost[i]);
+		 		}
+		 	}
+		 	
+		 	// 여벌이 있는 사람
+		 	for(int i = 0 ; i < n+1 ; i++) {
+		 		if(wear_on[i] == 1) {
+		 			sub_man.add(i);
+		 		}
+		 	}
+		 	
+		 	// 만약 체육복이 없는 사람이 빌릴 여벌이 없다면 명 수 체크
+		 	for(int i = 0 ; i < lost_man.size() ; i++) {
+		 		int num1 = sub_man.indexOf(lost_man.get(i) - 1);
+		 		int num2 = sub_man.indexOf(lost_man.get(i) + 1);
+		 		
+		 		// 1씩 작거나 클 때 여벌이 있는지 체크 (else > 여벌이 있다.)
+		 		if(num1 == -1) {
+		 			// 둘 다 없는 경우 체크
+		 			if(num2 == -1) {
+		 				no_wear += 1;
+		 			// 만약 존재시 여벌 리스트에서 삭제
+		 			}else {
+		 				sub_man.remove(num2);
+		 			}
+		 		}else {
+		 			sub_man.remove(num1);
+		 		}
+		 	}
+		 	
+		 	// 전체 인원에서 빼기
+	        int answer = n - no_wear;
+	        return answer;
+	    }
+
+
+}
---
+++ DAY05/P체육복/P체육복_조성환.java
@@ -0,0 +1,39 @@
+package tset;
+
+public class P체육복_조성환 {
+
+	public static void main(String[] args) {
+
+		String name = "JEROEN";
+		
+		System.out.println(solution(name));
+	}
+	
+    public static int solution(String name) {
+    	int answer = 0;
+    	// 좌우 최대 이동횟수
+    	int move_side = name.length() - 1;
+    	
+    	// 단어 길이 - 1 만큼 좌우 이동
+    	for(int i = 0 ; i < name.length(); i++) {
+    		// 왼쪽으로 이동 vs 우측으로 이동 (작은거 선택)
+    		// 아스키코드를 이용해 알파벳(= 'A' + 본인 숫자까지의 거리) - 'A' = 본인 숫자까지의 거리
+    		// 반대의 경우 26(알파벳 갯수) - 왼쪽으로의 거리 
+    		answer += Math.min(name.charAt(i) - 'A', 26 - (name.charAt(i) - 'A'));
+    		
+    		// 다음 인댁스 부터 탐색
+    		int indexA = i + 1;
+    		
+    		// 같은 글자라면 패스 
+			while(indexA < name.length() && name.charAt(indexA) == 'A') {
+				indexA++;
+			}
+			// i+i > 연속된 A문자열이 시작하기 전까지 갔다가 되돌아옴
+			move_side = Math.min(move_side, i + i + name.length() - indexA);
+			move_side = Math.min(move_side, (name.length() - indexA) * 2 + i);
+		}
+        
+    	// 총 숫자
+        return answer + move_side ;
+    }
+}
