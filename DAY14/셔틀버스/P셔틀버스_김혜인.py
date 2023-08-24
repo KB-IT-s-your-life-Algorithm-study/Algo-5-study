3845bfcaab6e0e56a852e901f64c5bdc085f468f
hen-ni <90090783+hen-ni@users.noreply.github.com>
2023-08-24 16:28:12 +0900

    Update P셔틀버스_김혜인.py

--- DAY14/셔틀버스/P셔틀버스_김혜인.py
+++ DAY14/셔틀버스/P셔틀버스_김혜인.py
@@ -1,1 +1,24 @@
 
+
+def solution(n, t, m, timetable):
+    answer = 0
+    # 크루 도착
+    crewTime = [int(time[:2])*60 + int(time[3:]) for time in timetable]
+    crewTime.sort()
+    # 버스 도착 
+    busTime = [9*60 + t*i for i in range(n)]
+
+    i = 0     
+    for tm in busTime:
+      cnt = 0   # 버스에 타는 크루 수
+      while cnt < m and i < len(crewTime) and crewTime[i] <= tm:
+        i += 1
+        cnt += 1
+
+      
+      # 버스에 자리가 남았을 경우
+      if cnt < m: answer = tm
+      # 버스에 자리가 없는 경우 맨 마지막 크루보다 1분 먼저 도착
+      else: answer = crewTime[i - 1] - 1
+
+    return str(answer//60).zfill(2) + ":" + str(answer%60).zfill(2)
